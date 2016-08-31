38800c9e23ab2fa15c78d34326faaf47ed9eb72c
Kyle Hebert <kyle.hebert@gmail.com>
2016-08-31 18:03:55 -0500

    tweaked JQuery to get a random joke from the API

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,31 +1,24 @@
 var jokeCount;
+var jokeId;
 
-function getJokeCount() {
+function getRandomJoke() {
+        // get the total number of jokes from the API
         $.ajax({
                    url: "http://localhost:8080/api/v1/jokes/"
                }).then(function(data) {
                 jokeCount = parseInt(data.page.totalElements);
-            console.log("Joke count is", jokeCount);
+                // get a random jokeId based on the total number of jokes
+                var minimumId = 1;
+                jokeId = Math.floor(Math.random() * (jokeCount - minimumId + 1 )) + minimumId;
+
+                //get a random joke and update the DOM
+                $.ajax({
+                           url: "http://localhost:8080/api/v1/jokes/" + jokeId
+                       }).then(function(data) {
+                    $('.joke-rank').append(data.rank);
+                    $('.joke-content').append(data.content);
+                });
         });
-};
-
-
-// returns a random number including the min and max
-function getRandomNumber() {
-    min = 1;
-    //TODO Need to use count from API instead of hardcoded max
-    max = 21;
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-};
-
-function getRandomJoke() {
-    var jokeId = getRandomNumber();
-    $.ajax({
-             url: "http://localhost:8080/api/v1/jokes/" + jokeId
-           }).then(function(data) {
-        $('.joke-rank').append(data.rank);
-        $('.joke-content').append(data.content);
-    });
 }
 
 getRandomJoke();
