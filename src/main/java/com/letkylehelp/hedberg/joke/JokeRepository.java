831a19d1e08194bde35253e86cc78bfb50152299
kylehebert <kyle.hebert@gmail.com>
2016-09-08 13:07:09 -0500

    Adds role-based permission for deleting jokes

--- src/main/java/com/letkylehelp/hedberg/joke/JokeRepository.java
+++ src/main/java/com/letkylehelp/hedberg/joke/JokeRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -20,4 +21,16 @@ public interface JokeRepository extends PagingAndSortingRepository<Joke, Long> {
   Page<Joke> findByContentContainingIgnoreCase(@Param("word") String word, Pageable pageable);
   @RestResource(rel = "get-top-five-jokes")
   List<Joke> findTop5ByOrderByRankAsc();
+
+
+//  Overriding these two delete methods only allows users with the
+//  Admin role to delete Jokes
+  @Override
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
+  void delete(Long id);
+
+  @Override
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
+  void delete(Joke entity);
+
 }
