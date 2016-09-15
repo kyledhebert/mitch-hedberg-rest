c73aa0f537099c38b0c81181166f83af3fd4d3ac
kylehebert <kyle.hebert@gmail.com>
2016-09-15 13:25:50 -0500

    Adds search for jokes based on words or phrases

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -2,27 +2,54 @@ var jokeCount;
 var jokeId;
 
 function getRandomJoke() {
-        // get the total number of jokes from the API
-        $.ajax({
-                   url: "http://localhost:8080/api/v1/jokes/"
-               }).then(function(data) {
-                jokeCount = parseInt(data.page.totalElements);
-                // get a random jokeId based on the total number of jokes
-                var minimumId = 1;
-                jokeId = Math.floor(Math.random() * (jokeCount - minimumId + 1 )) + minimumId;
+    // get the total number of jokes from the API
+    $.ajax({
+               url: "http://localhost:8080/api/v1/jokes/"
+           }).then(function (data) {
+        jokeCount = parseInt(data.page.totalElements);
+        // get a random jokeId based on the total number of jokes
+        var minimumId = 1;
+        jokeId = Math.floor(Math.random() * (jokeCount - minimumId + 1 )) + minimumId;
 
-                //get a random joke and update the DOM
-                $.ajax({
-                           url: "http://localhost:8080/api/v1/jokes/" + jokeId
-                       }).then(function(data) {
-                    $('.joke-rank').append("Rank " + data.rank);
-                    $('.joke-content').append(data.content);
-                });
+        //get a random joke and update the DOM
+        $.ajax({
+                   url: "http://localhost:8080/api/v1/jokes/" + jokeId
+               }).then(function (data) {
+            $('.joke-rank').append("Rank " + data.rank);
+            $('.joke-content').append(data.content);
         });
+    });
 }
 
 $(document).ready(function () {
-    $("#new-joke-button").click(function () {
+    var searchTerm;
+    // get search term on Enter key press
+    $('#search-term').keydown(function (e) {
+        var key = e.which;
+        if (key == 13) { // 13 = Enter
+            searchTerm = $('#search-term').val();
+            var url = 'http://localhost:8080/api/v1/jokes/search/containsWord?word=' + searchTerm;
+            $.getJSON(url, function (data) {
+                var html = "";
+                $.each(data._embedded, function (i, item) {
+                    $.each(item, function (j, joke) {
+                        html += "<div class='card-panel'>";
+                        html += "<p>" + joke.content + "</p>";
+                        html += "<p>Rank " + joke.rank + "</p>";
+                        html += "</div>";
+                    })
+                });
+
+                $('.joke').empty();
+                $('.joke-button').empty();
+                $('#jokes').html(html);
+            })
+        }
+    });
+});
+
+$(document).ready(function () {
+    $('#new-joke-button').click(function () {
         $('.joke-rank').empty();
         $('.joke-content').empty();
         getRandomJoke();
